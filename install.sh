03d6b9defde8dd2fa6f80baf1854765e4c7ce913
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2023-11-14 08:29:34 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ if [ -f "./.mountmedia-arch-confirmfile" ]; then
 sed -i "s|Exec.*|Exec=bash $PWD/mountinit.sh|g" ./mountmedia.desktop
 sed -i "s|Icon.*|Icon=$PWD/hdd.png|g" ./mountmedia.desktop
 sudo mv ./mountmedia.desktop /usr/share/applications/
-sed -i "s|./mountmedia.sh|$PWD/mountmedia.sh|g" ./mountinit.sh
+#sed -i "s|./mountmedia.sh|$PWD/mountmedia.sh|g" ./mountinit.sh
 printf "\e[1;32mDone\x21\e[1;33m\nPlease make sure you do not move this folder or the desktop launcher will not work. If you do, please update the desktop file in \e[1;39m/usr/share/applications/mountmedia.desktop\e[1;33m accordingly.\e[0m\n"
 
 else
